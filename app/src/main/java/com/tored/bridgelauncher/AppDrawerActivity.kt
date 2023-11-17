03c99b914d6c2519874abc210b51d0344f946f25
Tored <thetored@gmail.com>
2023-11-17 10:16:04 +0100

    solving stupid dropdown menu issues

--- app/src/main/java/com/tored/bridgelauncher/AppDrawerActivity.kt
+++ app/src/main/java/com/tored/bridgelauncher/AppDrawerActivity.kt
@@ -8,32 +8,58 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.LocalIndication
-import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
+import androidx.compose.ui.window.Popup
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import com.tored.bridgelauncher.composables.ResIcon
 import com.tored.bridgelauncher.ui.shared.SetSystemBarsForBotBarActivity
@@ -41,6 +67,10 @@ import com.tored.bridgelauncher.ui.theme.BridgeLauncherTheme
 import com.tored.bridgelauncher.ui.theme.botBar
 import com.tored.bridgelauncher.ui.theme.textSec
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 
 @AndroidEntryPoint
@@ -58,13 +88,46 @@ class AppDrawerActivity : ComponentActivity()
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AppDrawerScreen()
 {
     SetSystemBarsForBotBarActivity()
 
+    val context = LocalContext.current
+    val appContext = context.applicationContext as BridgeLauncherApp
+    val haptics = LocalHapticFeedback.current
+
     var searchString by remember { mutableStateOf("") }
+
+    var dropdownOpenFor by remember { mutableStateOf<InstalledApp?>(null) }
+    var dropdownItemInLazyColOffset by remember { mutableStateOf(Offset(0f, 0f)) }
+    var dropdownTouchOffset by remember { mutableStateOf(Offset(0f, 0f)) }
+    var dropdownParentSize by remember { mutableStateOf(IntSize(0, 0)) }
+
+    var dropdownFinalOffset = (dropdownItemInLazyColOffset + dropdownTouchOffset).toIntOffset()
+    val dropToLeft = dropdownFinalOffset.x > dropdownParentSize.width / 2
+    val dropUp = dropdownFinalOffset.y > dropdownParentSize.height / 2
+
+    val dropdownAlignment = if (dropToLeft)
+    {
+        if (dropUp)
+            Alignment.BottomEnd
+        else
+            Alignment.TopEnd
+    }
+    else
+    {
+        if (dropUp)
+            Alignment.BottomStart
+        else
+            Alignment.TopStart
+    }
+
+    if (dropToLeft)
+        dropdownFinalOffset = dropdownFinalOffset.copy(x = dropdownFinalOffset.x - dropdownParentSize.width)
+
+    if (dropUp)
+        dropdownFinalOffset = dropdownFinalOffset.copy(y = dropdownFinalOffset.y - dropdownParentSize.height)
 
     Surface(
         modifier = Modifier
@@ -74,102 +137,88 @@ fun AppDrawerScreen()
     {
         Column()
         {
-            val context = LocalContext.current
-            val appContext = context.applicationContext as BridgeLauncherApp
-            LazyColumn(
+            Box(
                 modifier = Modifier
-                    .weight(1f),
-                reverseLayout = true,
-                contentPadding = PaddingValues(0.dp, 8.dp),
+                    .weight(1f)
+                    .onGloballyPositioned {
+                        dropdownParentSize = it.size
+                    }
             )
             {
-                items(appContext.installedAppsHolder.installedApps)
-                { app ->
-                    var menuOpen by remember { mutableStateOf(false) }
-                    var menuOffsetX by remember { mutableFloatStateOf(0f) }
+                AppContextMenu(
+                    showForApp = dropdownOpenFor,
+                    offset = dropdownFinalOffset,
+                    alignment = dropdownAlignment,
+                    onDismissRequest = {
+                        dropdownOpenFor = null
+                    }
+                )
 
-                    Box()
-                    {
-                        DropdownMenu(
-                            expanded = menuOpen,
-                            onDismissRequest = { menuOpen = false },
-                            offset = DpOffset(LocalDensity.current.run { menuOffsetX.toDp() }, 0.dp)
-                        )
-                        {
-                            val clipman = LocalClipboardManager.current
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    reverseLayout = true,
+                    contentPadding = PaddingValues(0.dp, 8.dp),
+                )
+                {
+                    items(appContext.installedAppsHolder.installedApps)
+                    { app ->
 
-                            data class action(
-                                val iconResId: Int,
-                                val label: String,
-                                val onClick: () -> Unit,
-                            )
-
-                            val items = arrayOf(
-                                action(R.drawable.ic_copy, "Copy label")
-                                { clipman.setText(AnnotatedString(app.label)) },
-
-                                action(R.drawable.ic_copy, "Copy package name")
-                                { clipman.setText(AnnotatedString(app.packageName)) },
-
-                                action(R.drawable.ic_info, "App info")
-                                {
-                                    try
-                                    {
-                                        context.startActivity(
-                                            Intent(
-                                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                                                Uri.parse("package:${app.packageName}")
-                                            )
-                                        )
-                                    }
-                                    catch (_: Exception)
-                                    {
-                                        Toast.makeText(context, "Could not open app info.", Toast.LENGTH_SHORT).show()
-                                    }
-                                },
-
-                                action(R.drawable.ic_delete, "Uninstall")
-                                {
-                                    try
-                                    {
-                                        val packageURI = Uri.parse("package:${app.packageName}")
-                                        val uninstallIntent = Intent(Intent.ACTION_DELETE, packageURI)
-                                        context.startActivity(uninstallIntent)
-//                                        context.packageManager.packageInstaller.uninstall(app.packageName)
-                                    }
-                                    catch (_: Exception)
-                                    {
-                                        Toast.makeText(context, "Could not request app uninstall.", Toast.LENGTH_SHORT).show()
-                                    }
-                                },
-                            )
-
-                            for (item in items)
-                            {
-                                DropdownMenuItem(
-                                    content = {
-                                        ResIcon(item.iconResId)
-                                        Text(item.label, modifier = Modifier.padding(start = 12.dp, end = 8.dp))
-                                    },
-                                    onClick = {
-                                        item.onClick()
-                                    }
-                                )
-                            }
-                        }
+                        val interactionSource = remember { MutableInteractionSource() }
+                        var positionInParent by remember { mutableStateOf(Offset(0f, 0f)) }
 
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .defaultMinSize(minHeight = 48.dp)
-                                .combinedClickable(
-                                    onClick =  {
-                                        context.startActivity(app.launchIntent)
-                                    },
-                                    onLongClick = {
-                                        menuOpen = true
-                                    }
-                                )
+                                .indication(interactionSource, LocalIndication.current)
+                                .onGloballyPositioned {
+                                    positionInParent = it.positionInParent()
+                                }
+                                .pointerInput(Unit)
+                                {
+                                    detectTapGestures(
+                                        onPress = { offset ->
+                                            val press = PressInteraction.Press(offset)
+
+                                            val pressAfterDelayJob = CoroutineScope(coroutineContext).launch {
+                                                delay(100)
+                                                interactionSource.emit(press)
+                                            }
+
+                                            val gotCancelled = !tryAwaitRelease()
+                                            val wasNotEmitted = !pressAfterDelayJob.isCompleted
+
+                                            if (wasNotEmitted && gotCancelled)
+                                            {
+                                                pressAfterDelayJob.cancel()
+                                            }
+                                            else
+                                            {
+                                                if (wasNotEmitted)
+                                                    interactionSource.emit(press)
+
+                                                interactionSource.emit(
+                                                    if (gotCancelled)
+                                                        PressInteraction.Release(press)
+                                                    else
+                                                        PressInteraction.Cancel(press)
+                                                )
+                                            }
+                                        },
+
+                                        onTap = {
+                                            context.startActivity(app.launchIntent)
+                                        },
+
+                                        onLongPress = { offset ->
+                                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                            dropdownItemInLazyColOffset = positionInParent
+                                            dropdownTouchOffset = offset
+                                            dropdownOpenFor = app
+                                        }
+                                    )
+                                }
                                 .padding(8.dp, 0.dp),
                             verticalAlignment = Alignment.CenterVertically,
                             horizontalArrangement = Arrangement.spacedBy(8.dp)
@@ -196,7 +245,135 @@ fun AppDrawerScreen()
                     }
                 }
             }
+
             SearchBotBar(searchString) { searchString = it }
+        }
+    }
+}
+
+private fun Offset.toIntOffset(): IntOffset
+{
+    return IntOffset(this.x.toInt(), this.y.toInt())
+}
+
+@Composable
+fun AppContextMenu(
+    showForApp: InstalledApp?,
+    offset: IntOffset,
+    alignment: Alignment,
+    onDismissRequest: () -> Unit
+)
+{
+    val context = LocalContext.current
+    val clipman = LocalClipboardManager.current
+
+    data class Action(
+        val iconResId: Int,
+        val label: String,
+        val onClick: InstalledApp.() -> Unit,
+    )
+
+    val items = remember {
+        arrayOf(
+            Action(R.drawable.ic_copy, "Copy label")
+            { clipman.setText(AnnotatedString(label)) },
+
+            Action(R.drawable.ic_copy, "Copy package name")
+            { clipman.setText(AnnotatedString(packageName)) },
+
+            Action(R.drawable.ic_info, "App info")
+            {
+                try
+                {
+                    context.startActivity(
+                        Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:${packageName}")
+                        )
+                    )
+                }
+                catch (_: Exception)
+                {
+                    Toast.makeText(context, "Could not open app info.", Toast.LENGTH_SHORT).show()
+                }
+            },
+
+            Action(R.drawable.ic_delete, "Uninstall")
+            {
+                try
+                {
+                    val packageURI = Uri.parse("package:${packageName}")
+                    val uninstallIntent = Intent(Intent.ACTION_DELETE, packageURI)
+                    context.startActivity(uninstallIntent)
+                }
+                catch (_: Exception)
+                {
+                    Toast.makeText(context, "Could not request app uninstall.", Toast.LENGTH_SHORT).show()
+                }
+            },
+        )
+    }
+
+    @Composable
+    fun DropdownItems(app: InstalledApp)
+    {
+        for (item in items)
+        {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        item.onClick(app)
+                        onDismissRequest()
+                    }
+                    .padding(start = 32.dp, end = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.End),
+                verticalAlignment = Alignment.CenterVertically,
+            )
+            {
+                Text(
+                    item.label,
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                )
+                Box(
+                    modifier = Modifier
+                        .size(48.dp),
+                    contentAlignment = Alignment.Center,
+                )
+                {
+                    ResIcon(item.iconResId)
+                }
+            }
+        }
+    }
+
+    if (showForApp != null)
+    {
+        Popup(
+            offset = offset,
+            alignment = alignment,
+            onDismissRequest = onDismissRequest,
+        )
+        {
+
+            Surface(
+                modifier = Modifier,
+                color = MaterialTheme.colors.surface,
+                shape = MaterialTheme.shapes.large,
+                elevation = 8.dp,
+            )
+            {
+                Column(
+                    modifier = Modifier
+                        .width(IntrinsicSize.Min)
+                        .height(IntrinsicSize.Min)
+                        .padding(0.dp, 16.dp)
+                )
+                {
+
+                    DropdownItems(showForApp)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/tored/bridgelauncher/MainActivity.kt
+++ app/src/main/java/com/tored/bridgelauncher/MainActivity.kt
@@ -9,14 +9,30 @@ import android.view.WindowManager.LayoutParams
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,7 +40,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -187,7 +202,7 @@ fun BridgeButtonStateless(isExpanded: Boolean, onIsExpandedChange: (newState: Bo
             {
                 if (isExpanded)
                 {
-                                        TouchTarget(iconResId = R.drawable.ic_refresh) { }
+                    TouchTarget(iconResId = R.drawable.ic_refresh) { }
                     TouchTarget(iconResId = R.drawable.ic_dev_console) { }
 
                     Divider()
